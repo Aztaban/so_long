1aba42e262420f81b2f6a4f54cb7aee3cf2b8ea1
Martin Justa <mjusta@c2r7s7.42prague.com>
2025-06-22 14:48:17 +0200

    udpdated gnl in libft library

--- libft/src/gnl/get_next_line.c
+++ libft/src/gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mjusta <mjusta@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 17:41:24 by mjusta            #+#    #+#             */
-/*   Updated: 2025/06/13 18:31:14 by mjusta           ###   ########.fr       */
+/*   Updated: 2025/06/22 14:21:08 by mjusta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*get_next_line(int fd)
 	char		*buffer;
 	char		*next_line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (free_stash(&stash));
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
@@ -28,8 +28,6 @@ char	*get_next_line(int fd)
 	if (!stash || *stash == '\0')
 		return (free_stash(&stash));
 	next_line = extract_and_trim(&stash);
-	if (!next_line)
-		return (free_stash(&stash));
 	return (next_line);
 }
 
@@ -39,7 +37,7 @@ char	*get_next_line_multi_fd(int fd)
 	char		*buffer;
 	char		*next_line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || fd >= MAX_FD || BUFFER_SIZE <= 0)
 		return (free_stash(&stash[fd]));
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
@@ -49,26 +47,9 @@ char	*get_next_line_multi_fd(int fd)
 	if (!stash[fd] || *stash[fd] == '\0')
 		return (free_stash(&stash[fd]));
 	next_line = extract_and_trim(&stash[fd]);
-	if (!next_line)
-		return (free_stash(&stash[fd]));
 	return (next_line);
 }
 
-/* 
-#include <stdio.h>
-int	main(void)
-{
-	char *test_stash = ft_strdup("Hello\n42\nWorld");
-	//char *str = " of Warcraft";
-	//printf("%s World", extract_line(test_stash));
-
-	test_stash = trim_stash(test_stash);
-	
-	//test_stash = ft_strjoin_free_stash(test_stash, str);
-	printf("%s", test_stash);
-	free(test_stash);
-}
-*/
 /* 
 #include <stdio.h>
 #include <fcntl.h>
@@ -88,5 +69,41 @@ int	main(void)
 	}
 	
 	close(fd);
+	return (0);
+} */
+
+/* #include <stdio.h>
+#include <fcntl.h>
+int	main(void)
+{
+	int fd1 = open("test.txt", O_RDONLY);
+	int fd2 = open("test2.txt", O_RDONLY);
+	int fd3 = open("test3.txt", O_RDONLY);
+	
+	if (fd1 < 0 || fd2 < 0 || fd3 < 0)
+		return (1);
+	char *line1;
+	char *line2;
+	char *line3;
+	while (1)
+	{
+		line1 = get_next_line(fd1);
+		line2 = get_next_line(fd2);
+		line3 = get_next_line(fd3);
+		if (line1)
+			printf("Line 1: %s", line1);
+		if (line2)
+			printf("Line 2: %s", line2);
+		if (line3)
+			printf("Line 3: %s", line3);
+		if (!line1 && !line2 && !line3)
+			break;
+		free(line1);	
+		free(line2);	
+		free(line3);	
+	}
+	close(fd1);
+	close(fd2);
+	close(fd3);
 	return (0);
 } */
